5b9f4484fbffba7d63a1bf6df44ec0735cdc36e2
myraushanraj <raushan10695@gmail.com>
2019-12-22 13:11:26 +0530

    gg

--- bus.sql
+++ bus.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 30, 2018 at 06:28 AM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Dec 22, 2019 at 08:39 AM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -33,7 +33,7 @@ CREATE TABLE `admin` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `user_type` varchar(30) NOT NULL
+  `user_type` varchar(30) NOT NULL DEFAULT 'admin'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -41,7 +41,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `username`, `password`, `email`, `user_type`) VALUES
-(2, 'pattu', '49e654be128806a40728da361043a0fa', 'pattu@gmail.com', 'admin');
+(2, 'pattu', '49e654be128806a40728da361043a0fa', 'pattu@gmail.com', 'admin'),
+(20, 'raushan', '25d55ad283aa400af464c76d713c07ad', 'raushan@gmail.com', 'admin');
 
 -- --------------------------------------------------------
 
@@ -77,15 +78,17 @@ INSERT INTO `book_detail` (`seat_no`, `route_id`, `journey_date`, `booking_date`
 CREATE TABLE `bus_detail` (
   `bus_no` varchar(25) NOT NULL,
   `bus_type` varchar(50) NOT NULL,
-  `total_seat` int(11) NOT NULL
+  `total_seat` int(11) NOT NULL,
+  `bus_name` varchar(100) NOT NULL DEFAULT 'new india',
+  `staus` int(2) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `bus_detail`
 --
 
-INSERT INTO `bus_detail` (`bus_no`, `bus_type`, `total_seat`) VALUES
-('BA5KA1011', 'Deluxe', 32);
+INSERT INTO `bus_detail` (`bus_no`, `bus_type`, `total_seat`, `bus_name`, `staus`) VALUES
+('BA5KA1011', 'Deluxe', 32, 'new india', 1);
 
 -- --------------------------------------------------------
 
@@ -111,7 +114,8 @@ CREATE TABLE `customer` (
 INSERT INTO `customer` (`customer_id`, `username`, `password`, `address`, `city`, `gender`, `contact_no`, `email`) VALUES
 (1, 'uttammahat', '36b8d1ef000113045261277d75b72897', 'suryabianyak', 'suryabinayak', 'male', '9813100180', 'uttammahat@gmail.com'),
 (9, 'uttam', 'ccf8486cfd1f76323cb1c684b5d254fd', 'bkt', 'bkt', 'male', '9813100180', 'uttam@gmail.com'),
-(10, 'ram', '4641999a7679fcaef2df0e26d11e3c72', 'bkt', 'bkt', 'male', '9843130130', 'ram@gmail.com');
+(10, 'ram', '4641999a7679fcaef2df0e26d11e3c72', 'bkt', 'bkt', 'male', '9843130130', 'ram@gmail.com'),
+(11, 'raushan', '25f9e794323b453885f5181f1b624d0b', 'sdfs', 'delhi', 'male', '8109299136', 'raushan@monkhub.com');
 
 -- --------------------------------------------------------
 
@@ -155,13 +159,8 @@ CREATE TABLE `route_detail` (
 --
 
 INSERT INTO `route_detail` (`route_id`, `departure_station`, `arrival_station`, `via_station`, `rent`) VALUES
-(37, 'Banepa', 'Pokhara', 'Banepa', 1000),
-(39, 'Pokhara', 'Chitwan', 'Pokhara', 800),
-(40, 'Chitwan', 'Lumbini', 'Chitwan', 900),
-(41, 'Pokhara', 'Banepa', 'Pokhara', 1000),
-(42, 'Lumbini', 'Chitwan', 'Lumbini', 900),
-(43, 'Chitwan', 'Banepa', 'Chitwan', 900),
-(44, 'Chitwan', 'Pokhara', 'Chitwan', 800);
+(37, 'Delhi', 'Lakhimpur', 'Gola', 500),
+(39, 'Lakhimpur', 'Delhi', 'Gola', 500);
 
 -- --------------------------------------------------------
 
@@ -170,6 +169,7 @@ INSERT INTO `route_detail` (`route_id`, `departure_station`, `arrival_station`, 
 --
 
 CREATE TABLE `time_table` (
+  `time_table_id` int(11) NOT NULL,
   `route_id` int(11) NOT NULL,
   `departure_station` varchar(50) NOT NULL,
   `arrival_station` varchar(50) NOT NULL,
@@ -183,14 +183,9 @@ CREATE TABLE `time_table` (
 -- Dumping data for table `time_table`
 --
 
-INSERT INTO `time_table` (`route_id`, `departure_station`, `arrival_station`, `via_station`, `departure_time`, `arrival_time`, `rent`) VALUES
-(37, 'Banepa', 'Pokhara', 'Banepa', '07:00', '13:00', 1000),
-(39, 'Pokhara', 'Chitwan', 'Pokhara', '10:00', '15:00', 800),
-(40, 'Chitwan', 'Lumbini', 'Chitwan', '09:00', '15:00', 900),
-(41, 'Pokhara', 'Banepa', 'Pokhara', '07:00', '13:00', 1000),
-(42, 'Lumbini', 'Chitwan', 'Lumbini', '09:00', '15:00', 900),
-(43, 'Chitwan', 'Banepa', 'Chitwan', '08:00', '14:00', 900),
-(44, 'Chitwan', 'Pokhara', 'Chitwan', '10:00', '15:00', 800);
+INSERT INTO `time_table` (`time_table_id`, `route_id`, `departure_station`, `arrival_station`, `via_station`, `departure_time`, `arrival_time`, `rent`) VALUES
+(1, 37, 'Delhi', 'Lakhimpur', 'Gola', '20:00', '04:00', 500),
+(2, 39, 'Lakhimpur', 'Delhi', 'Gola', '19:00', '03:00', 500);
 
 --
 -- Indexes for dumped tables
@@ -231,6 +226,12 @@ ALTER TABLE `route_detail`
   ADD PRIMARY KEY (`route_id`);
 
 --
+-- Indexes for table `time_table`
+--
+ALTER TABLE `time_table`
+  ADD PRIMARY KEY (`time_table_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -238,7 +239,7 @@ ALTER TABLE `route_detail`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT for table `book_detail`
@@ -250,13 +251,19 @@ ALTER TABLE `book_detail`
 -- AUTO_INCREMENT for table `customer`
 --
 ALTER TABLE `customer`
-  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `route_detail`
 --
 ALTER TABLE `route_detail`
   MODIFY `route_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
+
+--
+-- AUTO_INCREMENT for table `time_table`
+--
+ALTER TABLE `time_table`
+  MODIFY `time_table_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
